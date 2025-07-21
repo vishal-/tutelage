bbda59955b87f5d62754d204e20fd7e804f3a069
Vishal Gupta <ims.vishal@gmail.com>
2025-07-21 19:31:54 +0530

    modified the title setup

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,9 +5,10 @@ type AppRoutesType = {
   [route: string]: {
     path: string;
     component: React.ComponentType;
+    title: string;
   };
 };
 export const AppRoutes: AppRoutesType = {
-  Home: { path: "/home", component: Home },
-  Time: { path: "/time", component: Time },
+  Home: { path: "/", component: Home, title: "Home" },
+  Time: { path: "/time", component: Time, title: "What time is it?" }
 };
